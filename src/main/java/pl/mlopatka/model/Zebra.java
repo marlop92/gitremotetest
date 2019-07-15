bd972ce0ad63ae34bbc2eac6869c9460adb20860
marcin.lopatka <marcin.lopatka@mercareon.com>
2019-07-15 14:32:55 +0200

    Add zebra

---
+++ src/main/java/pl/mlopatka/model/Zebra.java
@@ -0,0 +1,12 @@
+package pl.mlopatka.model;
+
+/**
+ * Created by marcin.lopatka on 15-07-2019
+ */
+public class Zebra extends Animal {
+	
+	@Override
+	public void say() {
+		System.out.println("Prr");
+	}
+}
