4924508a699f19b8fdf82d4d3719cf23356f90b9
marcin.lopatka <marcin.lopatka@mercareon.com>
2019-07-15 14:32:18 +0200

    Add lion

---
+++ src/main/java/pl/mlopatka/model/Lion.java
@@ -0,0 +1,11 @@
+package pl.mlopatka.model;
+
+/**
+ * Created by marcin.lopatka on 15-07-2019
+ */
+public class Lion extends Animal {
+	@Override
+	public void say() {
+		System.out.println("Roar");
+	}
+}
