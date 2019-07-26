006a47e29a3c2fe60976a616382ffe962a96d74c
marcin.lopatka <marcin.lopatka@mercareon.com>
2019-07-26 08:40:20 +0200

    Add tiger

---
+++ src/main/java/pl/mlopatka/model/Tiger.java
@@ -0,0 +1,12 @@
+package pl.mlopatka.model;
+
+/**
+ * Created by marcin.lopatka on 26-07-2019
+ */
+public class Tiger extends Animal {
+	
+	@Override
+	public void say() {
+		System.out.println("Wrrr!");
+	}
+}
