720a99b27e0a2dc9caa043d41270421507d6c51c
marcin.lopatka <marcin.lopatka@mercareon.com>
2019-07-26 08:41:31 +0200

    Add aardvark

---
+++ src/main/java/pl/mlopatka/model/Aardvark.java
@@ -0,0 +1,12 @@
+package pl.mlopatka.model;
+
+/**
+ * Created by marcin.lopatka on 26-07-2019
+ */
+public class Aardvark extends Animal {
+	
+	@Override
+	public void say() {
+		System.out.println("Snorf!");
+	}
+}
