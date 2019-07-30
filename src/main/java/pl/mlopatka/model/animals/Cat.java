1e1a21710451bb5d18162c5fbbe60293ea7fa130
marcin.lopatka <marcin.lopatka@mercareon.com>
2019-07-30 09:02:02 +0200

    Modfiy cat

--- src/main/java/pl/mlopatka/model/animals/Cat.java
+++ src/main/java/pl/mlopatka/model/animals/Cat.java
@@ -3,5 +3,9 @@ package pl.mlopatka.model.animals;
 /**
  * Created by marcin.lopatka on 30-07-2019
  */
-public class Cat {
+public class Cat extends Animal {
+	@Override
+	public void say() {
+		System.out.println("Miau!");
+	}
 }
